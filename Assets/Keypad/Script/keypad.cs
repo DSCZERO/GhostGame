e173a951313f6d75a3b5d60e3c089393cd2e99ce
thomastra <thomas.tra12345@gmail.com>
2025-03-26 21:06:30 +1100

    added door keycode interaction

---
+++ Assets/Keypad/Script/keypad.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class keypad : MonoBehaviour
+{
+    public GameObject objectToEnable;
+    public GameObject objectToDestroy;
+
+    [Header("Keypad Settings")]
+    public string curPassword = "123";
+    public string input;
+    public Text displayText;
+    public AudioSource audioData;
+
+    //Local private variables
+    private bool keypadScreen;
+    private float btnClicked = 0;
+    private float numOfGuesses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnClicked = 0; // No of times the button was clicked
+        numOfGuesses = curPassword.Length; // Set the password length.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (btnClicked == numOfGuesses)
+        {
+            if (input == curPassword)
+            {
+                Debug.Log("Correct Password!");
+                
+                if (objectToDestroy != null)
+                {
+                    objectToDestroy.SetActive(false);
+                }
+                input = "";
+                btnClicked = 0;
+
+            }
+            else
+            {
+                //Reset input varible
+                input = "";
+                displayText.text = input.ToString();
+                audioData.Play();
+                btnClicked = 0;
+            }
+
+        }
+
+    }
+
+    void OnGUI()
+    {
+        // Action for clicking keypad on screen
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                var selection = hit.transform;
+
+                if (selection.CompareTag("keypad"))
+                {
+                    keypadScreen = true;
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+
+                    var selectionRender = selection.GetComponent<Renderer>();
+                    if (selectionRender != null)
+                    {
+                        keypadScreen = true;
+                    }
+                }
+
+            }
+        }
+
+        if (keypadScreen)
+        {
+            objectToEnable.SetActive(true);
+        }
+
+    }
+
+    public void ValueEntered(string valueEntered)
+    {
+        switch (valueEntered)
+        {
+            case "Q": // QUIT
+                objectToEnable.SetActive(false);
+                btnClicked = 0;
+                keypadScreen = false;
+                input = "";
+                displayText.text = input.ToString();
+                Cursor.visible = false;
+                break;
+
+            case "C": //CLEAR
+                input = "";
+                btnClicked = 0;
+                displayText.text = input.ToString();
+                break;
+
+            default: // Buton clicked add a variable
+                btnClicked++;
+                input += valueEntered;
+                displayText.text = input.ToString();
+                break;
+        }
+
+
+    }
+}
