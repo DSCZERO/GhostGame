eff2fe278d1902ecdf409e4c053866a50587470b
AishKunwar <aishkunwar2012@gmail.com>
2025-04-22 15:01:18 +1000

    Added telekinesis to tutorial level

--- Assets/Scripts/Telekinesis.cs
+++ Assets/Scripts/Telekinesis.cs
@@ -6,24 +6,49 @@ using UnityEngine.SceneManagement;
 
 public class Telekenisis : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float range = 10f;
+    public float moveSpeed = 5f;
+    public Camera playerCamera;
 
-    // Update is called once per frame
+    private Transform targetedObject;
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.T)) {
-            
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (targetedObject == null)
+                TryFocusOnObject();
+
+            if (targetedObject != null)
+                MoveObjectWithInput();
+        }
+        else
+        {
+            targetedObject = null; // Release object when Q is released
         }
     }
 
-    void OnTriggerEnter (Collider other)
+    void TryFocusOnObject()
     {
-        Debug.Log("You've entered telekenis range");
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        if (Physics.Raycast(ray, out RaycastHit hit, range))
+        {
+            if (hit.transform.CompareTag("Telekinesis"))
+            {
+                targetedObject = hit.transform;
+            }
+        }
+    }
 
+    void MoveObjectWithInput()
+    {
+        Vector3 direction = Vector3.zero;
 
+        if (Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back;
+        if (Input.GetKey(KeyCode.LeftArrow)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow)) direction += Vector3.right;
+
+        targetedObject.position += direction * moveSpeed * Time.deltaTime;
     }
 }
